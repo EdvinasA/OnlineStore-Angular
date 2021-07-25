40211698494007ee002947ba05096d650d8c2c94
Edvinas <edvinasalimas98@gmail.com>
2021-07-25 18:45:30 +0300

    First commit

--- online-store-angular-frontEnd/src/app/app.component.spec.ts
+++ online-store-angular-frontEnd/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'online-store-angular-front'`, () => {
+  it(`should have as title 'online-store-angular-frontEnd'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('online-store-angular-front');
+    expect(app.title).toEqual('online-store-angular-frontEnd');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('online-store-angular-front app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('online-store-angular-frontEnd app is running!');
   });
 });
