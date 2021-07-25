505bb3f6f9dd76fd97d08746eb9001912ff01cdf
Edvinas <edvinasalimas98@gmail.com>
2021-07-25 18:56:24 +0300

    Initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'online-store-angular-frontEnd'`, () => {
+  it(`should have as title 'OnlineStore-Angulars'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('online-store-angular-frontEnd');
+    expect(app.title).toEqual('OnlineStore-Angulars');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('online-store-angular-frontEnd app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('OnlineStore-Angulars app is running!');
   });
 });
